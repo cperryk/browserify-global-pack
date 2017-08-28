9a0164543ae56c334d652d4768cedeb94eb3def8
Chris Kirk <cperryk@gmail.com>
2017-08-28 16:14:51 -0400

    Refactor for cleaner code

--- index.js
+++ index.js
@@ -6,27 +6,87 @@ const globalPack = require('global-pack'),
   highland = require('highland'),
   writeFile = highland.wrapCallback(fs.writeFile);
 
-function packAndWrite(writeToDir, opts) {
-  const ids = [];
-  let i = 0;
+/**
+ * Returns a stream that takes in deps from a Browserify
+ * bundle process, runs them through global-pack, and
+ * writes each module out to a file at the specified
+ * destination directory.
+ * @param {string} writeToDir
+ * @param {object} [opts]
+ * @param {string} [opts.scope] global-pack scope
+ * @param {string} [opts.verbose] log file writes
+ */
+function packAndWrite(writeToDir, {scope, verbose} = {}) {
+  const deps = [];
 
   return highland.pipeline(
-    highland.map((dep) => {
-      if (dep.file === dep.id) {
-        ids.push(pathUtil.parse(dep.id).name);
-      } else {
-        ids.push(dep.id);
-      }
-      return dep;
-    }),
-    globalPack(opts),
-    highland.flatMap((str) => {
-      const filename = (i ? ids[i - 1] || 'postlude' : 'prelude') + '.js',
-        outpath = pathUtil.join(writeToDir, filename);
+    highland.doto(({file, id}) => deps.push({file, id})),
+    globalPack({scope}),
+    getFiles(deps, writeToDir),
+    writeFiles(verbose)
+  );
+};
 
-      i++;
-      return writeFile(outpath, str + '\n');
-    })
+/**
+ * Returns a stream that determines where each module in a global-pack
+ * bundle should export, taking in global-pack strings and outputting
+ * {id: string, outpath: string, content: string} objects.
+ * @param {string[]} deps Array of module IDs
+ * @param {string[]} writeToDir
+ * @return {Stream}
+ */
+function getFiles(deps, writeToDir) {
+  let i = 0;
+
+  return highland.map((str) => {
+    const content = str + '\n',
+      dep = deps[i - 1];
+    let id, sourceFile;
+
+    if (i === 0) {
+      id = 'prelude';
+      sourceFile = '(prelude)';
+    } else if (dep) {
+      id = dep.id;
+      sourceFile = dep.file;
+    } else {
+      id = 'postlude';
+      sourceFile = '(postlude)';
+    }
+    i++;
+
+    return {
+      id,
+      sourceFile,
+      outpath: getOutpath(id, writeToDir),
+      content
+    };
+  });
+}
+
+/**
+ * For a given module ID and write destination, return the absolute filepath
+ * to which the module should be written.
+ * @param {string} id
+ * @param {string} writeToDir
+ * @return {string}
+ */
+function getOutpath(id, writeToDir) {
+  if (pathUtil.isAbsolute(id)) {
+    return pathUtil.join(writeToDir, pathUtil.parse(id).name + '.js');
+  }
+  return pathUtil.join(writeToDir, id + '.js');
+}
+
+/**
+ * Returns a stream that takes in {outpath: string, content: string} objects
+ * and writes files.
+ * @param {boolean} verbose
+ * @return {Stream}
+ */
+function writeFiles(verbose) {
+  return highland.flatMap(file => writeFile(file.outpath, file.content)
+    .doto(() => verbose && console.log(`${file.sourceFile} -> ${file.outpath}`))
   );
 }
 
